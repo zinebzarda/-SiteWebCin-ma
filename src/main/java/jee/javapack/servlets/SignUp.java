29c14097025475e24810860cc8ba32a3c5c7adeb
Noureddine Bouzire <bouzire.noureddine1@gmail.com>
2024-04-27 22:24:01 +0100

    Creating the SignUp servlet

--- src/main/java/jee/javapack/servlets/SignUp.java
+++ src/main/java/jee/javapack/servlets/SignUp.java
@@ -1,18 +1,40 @@
 package jee.javapack.servlets;
-import javax.servlet.*;
-import javax.servlet.http.*;
-import javax.servlet.annotation.*;
+
+import jee.javapack.dao.SignInDAO;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.SQLException;
 
-@WebServlet(name = "SignUp", value = "/SignUp")
+@WebServlet("/signUp")
 public class SignUp extends HttpServlet {
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    private static final long serialVersionUID = 1L;
 
-    }
-
-    @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String userName = request.getParameter("name");
+        String userEmail = request.getParameter("email");
+        String userPassword = request.getParameter("password");
+        RequestDispatcher dispatcher = null;
 
+        try {
+            int counter = SignInDAO.createUser(userName, userEmail, userPassword);
+            dispatcher = request.getRequestDispatcher("authentication.jsp");
+
+            if (counter > 0) {
+                request.setAttribute("status", "success");
+            } else {
+                request.setAttribute("status", "failed");
+            }
+
+            dispatcher.forward(request, response);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            // Handle the error appropriately, maybe redirect to an error page
+        }
     }
 }
