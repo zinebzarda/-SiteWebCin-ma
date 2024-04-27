2256acf003855b1ec6fa691cbe97c7896ee3b96e
Noureddine Bouzire <bouzire.noureddine1@gmail.com>
2024-04-27 22:38:20 +0100

    adding the Login servlet

--- src/main/java/jee/javapack/servlets/Login.java
+++ src/main/java/jee/javapack/servlets/Login.java
@@ -1,18 +1,48 @@
 package jee.javapack.servlets;
-import javax.servlet.*;
-import javax.servlet.http.*;
-import javax.servlet.annotation.*;
+
+import jee.javapack.dao.LoginDAO;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.sql.SQLException;
 
-@WebServlet(name = "Login", value = "/Login")
+@WebServlet("/Login")
 public class Login extends HttpServlet {
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    private static final long serialVersionUID = 1L;
 
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        String login = request.getParameter("login");
+        String password = request.getParameter("pwd");
+
+        try {
+            boolean isValidUser = LoginDAO.validateUser(login, password);
+            System.out.println("Login Attempt: Email - " + login + ", Password - " + password);
+            if (isValidUser) {
+                session.setAttribute("login", login);
+                String role = getUserRole(login, password);
+                if ("admin".equals(role)) {
+                    response.sendRedirect("admin.jsp");
+                } else {
+                    response.sendRedirect("CinemaHome.jsp");
+                }
+            } else {
+                response.sendRedirect("authentication.jsp");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            // Handle the error appropriately, maybe redirect to an error page
+        }
     }
 
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+    private String getUserRole(String login, String password) throws SQLException {
+        String userRole = "";
+        // You can reuse the code to get the user role from the database
+        return userRole;
     }
 }
