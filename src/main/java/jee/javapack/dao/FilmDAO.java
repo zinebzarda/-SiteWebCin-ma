146c1b39e623b91778ef4404b05a8a5db388ef97
YassineOularbi <157104579+YassineOularbi@users.noreply.github.com>
2024-04-28 19:52:49 +0100

    Update FilmDAO.java

--- src/main/java/jee/javapack/dao/FilmDAO.java
+++ src/main/java/jee/javapack/dao/FilmDAO.java
@@ -8,18 +8,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public interface FilmDAO {
-<<<<<<< HEAD
+
     ArrayList<Film> SearchFilms(String Title) throws SQLException, ClassNotFoundException;
-    List<Film> getAllFilms() throws SQLException, ClassNotFoundException;
+    List<Film> getAllFilms();
     List<Film> getHighRatedFilms();
+    Film getMovieById(Integer idMovie) throws SQLException, ClassNotFoundException;
     void addFilms(Film film) throws SQLException;
     ArrayList<Film> showFilm()  throws SQLException;
     void updateFilm(Film film) throws SQLException;
     void deleteFilm(int idFilm) throws SQLException;
-=======
-    List<Film> getAllFilms();
-    List<Film> getHighRatedFilms();
-    Film getMovieById(Integer idMovie) throws SQLException, ClassNotFoundException;
-    ArrayList<Film> SearchFilms(String Title) throws SQLException, ClassNotFoundException;
->>>>>>> 7a8dc9b08099e6affebef5e4db0cbb67eb5b18fe
 }
