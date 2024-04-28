0d8bb2554cfa4a7d36f1ad48bd4296a22291d1db
YassineOularbi <157104579+YassineOularbi@users.noreply.github.com>
2024-04-28 19:56:13 +0100

    Update CinemaHome.java

--- src/main/java/jee/javapack/servlets/CinemaHome.java
+++ src/main/java/jee/javapack/servlets/CinemaHome.java
@@ -18,19 +18,7 @@ public class CinemaHome extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         List<Film> ratingFilms = filmDAO.getHighRatedFilms();
         request.setAttribute("ratingFilms", ratingFilms);
-
-<<<<<<< HEAD
-        List<Film> films;
-        try {
-            films = filmDAO.getAllFilms();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-=======
         List<Film> films = filmDAO.getAllFilms();
->>>>>>> 7a8dc9b08099e6affebef5e4db0cbb67eb5b18fe
         request.setAttribute("films", films);
         System.out.println(films);
         request.getRequestDispatcher("/CinemaHome.jsp").forward(request, response);
@@ -39,10 +27,8 @@ public class CinemaHome extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-<<<<<<< HEAD
-        doGet(request,response);
-=======
 
->>>>>>> 7a8dc9b08099e6affebef5e4db0cbb67eb5b18fe
+        doGet(request,response);
+
     }
 }
